3a167a397a4f75b436cb16ae9cadd71ee8eb43f6
kostya2 <kmingoulin@gmail.com>
2015-09-11 19:13:47 -0400

    working Plot1

--- Plot1.R
+++ Plot1.R
@@ -1,13 +1,21 @@
 #plot1.R
 library(lubridate)
+library(dplyr)
 
-#C:/Users/kostya2/Documents/R/DS/EX
+
 data1<-read.csv("C:/Users/kostya2/Documents/R/DS/EX/household_power_consumption.txt",
                 header = TRUE,sep=";",na.strings = "?", stringsAsFactors = FALSE)
 
 data1$Time<-hms(data1$Time)
 data1$Date<-dmy(data1$Date)
 
-data_work<- subset(data1, Date >= mdy("02/01/2007") & Date <= mdy("02/02/2007")) 
+
+
+
+dw<-subset(data1, Date >=ymd("2007-02-01") & Date<=ymd("2007-02-02"))
+#dw<-mutate(dw, agpKW=Global_reactive_power*10)
 rm(data1)
 
+
+hist(dw$Global_active_power,col="red",main ="Global Active Power",
+     xlab="Global Active Power (kilowatts)") 
